4d02e920f0ad674ea190a6066c5e2ed79ed752a3
lukas <lukas@lukas-VirtualBox>
2015-11-02 23:21:39 +0000

    jjj

--- MorseCode/src/DB_read.java
+++ MorseCode/src/DB_read.java
@@ -12,6 +12,7 @@ public class DB_read {
    static final String DB_URL = "jdbc:mysql://localhost:3306";
 
    //  Database credentials
+// Database credentials
    static final String USER = "root";
    static final String PASS = "root";
    
